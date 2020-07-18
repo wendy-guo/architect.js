8a6b1db3253e82005b2a08da48fc1f4afb7c9c6b
wnygby <wny.guo@mail.utoronto.ca>
2020-07-18 12:27:06 -0400

    fixed shaders and updated blocks to draw cubes

--- shaders.js
+++ shaders.js
@@ -1,23 +1,23 @@
 const vertexShaderSource = `
     attribute vec4 a_position;  // position coordinates
-    attribute vec4 a_color;  // pixel colour
+    attribute vec4 a_colour;  // pixel colour
 
     uniform mat4 u_matrix;  // transformation matrix
 
-    varying vec4 v_color;
+    varying vec4 v_colour;
 
     void main() {
         gl_Position = u_matrix * a_position;
-        v_color = a_color;
+        v_colour = a_colour;
     }`;
 
 const fragmentShaderSource = `
     precision mediump float;
 
-    varying vec4 v_color;
+    varying vec4 v_colour;
 
     void main() {
-        gl_FragColor = v_color;
+        gl_FragColor = v_colour;
     }`;
 
 export { vertexShaderSource, fragmentShaderSource };
