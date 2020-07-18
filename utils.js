5965aeaf8583f349ce7d223ebcafa4242c7c570c
wnygby <wny.guo@mail.utoronto.ca>
2020-07-18 16:45:41 -0400

    moved util. functions to new file

---
+++ utils.js
@@ -0,0 +1,86 @@
+const utils = {
+    _createShader: (gl, type, source) => {
+        let shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+
+        if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            return shader; // success
+        }
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+    },
+    
+    _setGeometry: (gl, geometry) => {
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array(geometry),
+            gl.STATIC_DRAW);
+    },
+
+    _setColours: (gl, colours) => {
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Uint8Array(colours),
+            gl.STATIC_DRAW);
+    },
+
+    setupProgram: (gl, vertexShaderSource, fragmentShaderSource) => {
+        let program = gl.createProgram();
+        let vertexShader = utils._createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+        let fragmentShader = utils._createShader(
+            gl,
+            gl.FRAGMENT_SHADER,
+            fragmentShaderSource
+        );
+
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+
+        if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            return program; // success
+        }
+        console.log(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    },
+
+    resize: (canvas) => {
+        if (
+            canvas.width != canvas.clientWidth ||
+            canvas.height != canvas.clientHeight
+        ) {
+            canvas.width = canvas.clientWidth;
+            canvas.height = canvas.clientHeight;
+        }
+    },
+
+    setupBufferAttribPointers: (gl, block) => {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, block.positionBuffer);
+        utils._setGeometry(gl, block.geometry);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, block.colourBuffer);
+        utils._setColours(gl, block.colours);
+
+        console.log("setup buffer data");
+    },
+
+    enableBufferAttribPointers: (gl, program, block) => {
+        let positionA = gl.getAttribLocation(program, "a_position");
+        let colourA = gl.getAttribLocation(program, "a_colour");
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, block.positionBuffer);
+        gl.vertexAttribPointer(positionA, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(positionA);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, block.colourBuffer);
+        gl.vertexAttribPointer(colourA, 3, gl.UNSIGNED_BYTE, true, 0, 0);
+        gl.enableVertexAttribArray(colourA);
+
+        console.log("enabled buffers");
+
+    }
+};
+
+export { utils };
