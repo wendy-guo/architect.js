57fdc09f5609399b932d8db764bb8d16223ed921
wnygby <wny.guo@mail.utoronto.ca>
2020-07-25 13:12:16 -0400

    updated shaders to set colour based on lighting

--- shaders.js
+++ shaders.js
@@ -1,23 +1,30 @@
 const vertexShaderSource = `
     attribute vec4 a_position;  // position coordinates
-    attribute vec4 a_colour;  // pixel colour
+    attribute vec3 a_normal;  // normal
 
     uniform mat4 u_matrix;  // transformation matrix
 
-    varying vec4 v_colour;
+    varying vec3 v_normal;
 
     void main() {
         gl_Position = u_matrix * a_position;
-        v_colour = a_colour;
+        v_normal = a_normal;
     }`;
 
 const fragmentShaderSource = `
     precision mediump float;
 
-    varying vec4 v_colour;
+    varying vec3 v_normal;
+
+    uniform vec3 u_reverseLight;
+    uniform vec4 u_colour;
 
     void main() {
-        gl_FragColor = v_colour;
+        vec3 normal = normalize(v_normal);  // convert to unit vector
+        float light = dot(normal, u_reverseLight);  // get light direction
+
+        gl_FragColor = u_colour;
+        gl_FragColor.rgb *= light;
     }`;
 
 export { vertexShaderSource, fragmentShaderSource };
