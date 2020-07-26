7f853b7a64dc400b7932106a92cbdbe4142a5f12
wnygby <wny.guo@mail.utoronto.ca>
2020-07-26 12:45:59 -0400

    simplified drawScene to use helpers

--- pub/js/architect/app.js
+++ pub/js/architect/app.js
@@ -1,78 +1,101 @@
-import { vertexShaderSource, fragmentShaderSource } from "./shaders.js";
-import { degToRad, radToDeg } from "./angles.js";
-import { threeD } from "./three-d.js";
-import { utils } from "./utils.js";
-import { Block } from "./blocks.js";
+import { vertexShaderSource, fragmentShaderSource } from "./architect/shaders.js";
+import { degToRad, radToDeg } from "./architect/angles.js";
+import { threeD } from "./architect/three-d.js";
+import { utils } from "./architect/utils.js";
+import { Architect } from "./architect/architect.js";
+import { gridWaves } from "./architect/grid-waves.js";
 
 ("use strict");
 const log = console.log;
+log("hello architect ^^");
 
-function main() {
+function main(cv) {
+
   // ..........................................................
   // get canvas element and webgl context
-  const canvas = document.getElementById("canvas");
-  const gl = canvas.getContext("webgl");
+  const canvas = document.getElementById(cv);
+  const gl = Architect.getContext(canvas);
 
   if (!gl) {
-    console.log("trying experimental");
+    console.log("no webgl");
     return;
   }
 
-  // ...........................................................
-  // create program with shaders
-  let program = utils.setupProgram(gl, vertexShaderSource, fragmentShaderSource);
+  const program = Architect.getProgram();
 
-  // ............................................................
-  // lookup attributes, uniforms, and varyings
-  let matrixU = gl.getUniformLocation(program, "u_matrix");
+  const c1 = [[248, 196, 205],
+  [211, 137, 149],
+  [211, 137, 149],
+  [253, 231, 205],
+  [211, 137, 149],
+  [248, 196, 205]];
 
-  // ............................................................ 
-  // create block objects
-  const block = new Block(gl, 50);
-  utils.setupBufferAttribPointers(gl, block);
 
-  const moreBlocks = [];
-  for (let i = 0; i < 3; i++) {
-    let b = new Block(gl, 40);
-    utils.setupBufferAttribPointers(gl, b);
-    moreBlocks.push(b);
-  }
+  const grid = new Architect.BlocksGrid(gl, 15, 15, [20, 20, 20], [-50, 150, 0], c1);
+
+  //const stairs = new Stairs(gl, 10, [20, 20, 100], [150, 100, -50]);
 
   // ........................................................... 
   // initial transformation settings
-  let translation = [200, 150, 0];
-  let rotation = [degToRad(50), degToRad(200), degToRad(100)];
+  let translation = [0, 0, -1000];
+  let rotation = [degToRad(30), degToRad(-30), degToRad(0)];
   let scale = [1, 1, 1];
+  let fieldOfViewRadians = degToRad(35);
+  let cameraRotation = 0;
+  let view;
+  let angle = 0;
 
   // ............................................................ 
   // draw scene
-  drawScene();
+  requestAnimationFrame(drawScene); // Setup a ui.
 
   function drawScene() {
 
-    utils.resize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    utils.setSettings(gl);
-
+    utils.resetGL(gl);
     gl.useProgram(program);
 
-    utils.enableBufferAttribPointers(gl, program, block);
+    updateView();
 
-    let matrix = threeD.project(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
-    matrix = threeD.transform(matrix, translation, rotation, scale);
-    gl.uniformMatrix4fv(matrixU, false, matrix);
+    let matrix = threeD.transform(view, translation, rotation, scale);
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);  // number of points (every 3 points draws 1 triangle)
+    let offsetR = 0;
+    let offsetC = 0;
 
-    for (let i = 0; i < 3; i++){
-      matrix = threeD.translate(matrix, 50, 50, 0);
-      gl.uniformMatrix4fv(matrixU, false, matrix);
+    for (let r = 0; r < grid.rows; r++) {
+      for (let c = 0; c < grid.columns; c++) {
+        let block = grid.blocks[r][c];
 
-      utils.enableBufferAttribPointers(gl, program, moreBlocks[i]);
-      gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);  // number of points (every 3 points draws 1 triangle)
+        let m = threeD.translate(matrix, block.position[0], block.position[1], block.position[2]);
+        m = threeD.scale(m, 1, getScaleFactor(angle + offsetR + offsetC), 1, 25);
+        block.setMatrix(m);
+
+        utils.setupBufferAttribPointers(gl, block);
+        utils.enableAttribUniform(gl, program, block);
+        gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);  // number of points (every 3 points draws 1 triangle)});
+        offsetC += 0.2;
+      }
+      offsetR += 0.2;
     }
+
+    angle += 0.05;
+    requestAnimationFrame(drawScene);
+  }
+
+  function updateView() {
+    let projection = threeD.perspective(fieldOfViewRadians, gl.canvas.clientWidth / gl.canvas.clientHeight, 1, 2000);
+    let camera = threeD.yRotationMatrix(cameraRotation);
+    camera = threeD.translate(camera, 0, 0, 100);
+
+    view = threeD.inverse(camera);
+    view = threeD.matrixMultiply(projection, view);
+  }
+
+  function getScaleFactor(angle) {
+    let scaleFactor = Math.sin(angle) + 1;  // sin goes from -1 -> 1, changing to 0 -> 2
+    scaleFactor *= 4 // 0 -> 8
+    scaleFactor += 3;  // 3 -> 11
+    return scaleFactor;
   }
 }
 
-main();
+main("canvas");
