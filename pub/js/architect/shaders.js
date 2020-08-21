1ad054783fa1be64f266cef7db814d0dee9e2d2d
wnygby <wny.guo@mail.utoronto.ca>
2020-08-21 14:21:49 -0400

    updated shaders to fix lighting

--- pub/js/architect/shaders.js
+++ pub/js/architect/shaders.js
@@ -12,12 +12,8 @@ const vertexShaderSource = `
 
     void main() {
         gl_Position = u_matrix * a_position;
-        if (a_lighting > 0.5) {
-            v_normal = a_normal;
-        } else {
-            v_colour = a_colour;
-        }
-        v_lighting = a_lighting;
+        v_colour = a_colour;
+
     }`;
 
 const fragmentShaderSource = `
@@ -32,37 +28,24 @@ const fragmentShaderSource = `
 
     void main() {
 
-        if (v_lighting > 0.5) {
-            vec3 normal = normalize(v_normal);  // convert to unit vector
-            float light = dot(normal, u_reverseLight);  // get light direction
-
-            gl_FragColor = u_colour;
-            gl_FragColor.rgb *= light;
-        } else {
-            gl_FragColor = v_colour;
-        }
+        gl_FragColor = v_colour;
+        
     }`;
 
 const vertexShaderSourceWithLighting = `
     attribute vec4 a_position;  // position coordinates
     attribute vec3 a_normal;  // normal
     attribute vec4 a_colour;  // colour for each face
-    attribute float a_lighting;  // lighting (used as boolean)
 
     uniform mat4 u_matrix;  // transformation matrix
 
     varying vec3 v_normal;
     varying vec4 v_colour;
-    varying float v_lighting;
 
     void main() {
         gl_Position = u_matrix * a_position;
-        if (a_lighting > 0.5) {
-            v_normal = a_normal;
-        } else {
-            v_colour = a_colour;
-        }
-        v_lighting = a_lighting;
+        v_normal = a_normal;
+        v_colour = a_colour;
     }`;
 
 const fragmentShaderSourceWithLighting = `
@@ -70,22 +53,17 @@ const fragmentShaderSourceWithLighting = `
 
     varying vec3 v_normal;
     varying vec4 v_colour;
-    varying float v_lighting;
 
     uniform vec3 u_reverseLight;
-    uniform vec4 u_colour;
 
     void main() {
 
-        if (v_lighting > 0.5) {
-            vec3 normal = normalize(v_normal);  // convert to unit vector
-            float light = dot(normal, u_reverseLight);  // get light direction
+        //vec3 normal = normalize(v_normal);  // convert to unit vector
+        float light = dot(v_normal, u_reverseLight);  // get light direction
 
-            gl_FragColor = u_colour;
-            gl_FragColor.rgb *= light;
-        } else {
-            gl_FragColor = v_colour;
-        }
+        gl_FragColor = v_colour;
+        gl_FragColor.rgb *= light + 0.3;
+
     }`;
 
 export { vertexShaderSource, fragmentShaderSource, vertexShaderSourceWithLighting, fragmentShaderSourceWithLighting };
